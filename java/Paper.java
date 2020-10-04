a38469e33954a9a65e81e27240a4b52430dfd158
caiman <me@caiman.dev>
2020-10-04 13:54:02 +0300

    [develop] Create Pen and Paper classes

---
+++ java/Paper.java
@@ -0,0 +1,39 @@
+public class Paper {
+    private StringBuffer content = new StringBuffer();
+    private int maxSymbols;
+    private int symbols;
+
+    public Paper(int maxSymbols) {
+        this.maxSymbols = maxSymbols;
+        this.symbols = 0;
+    }
+
+    public Paper() {
+        this(4096);
+    }
+
+    public int getMaxSymbols() {
+        return this.maxSymbols;
+    }
+
+    public int getSymbols() {
+        return this.symbols;
+    }
+
+    public void addContent(String message) throws OutOfSpaceException {
+        int total = this.content.length() + message.length();
+
+        if ( this.content.length() == this.maxSymbols ) {
+            throw new OutOfSpaceException();
+        }
+        if ( total > this.maxSymbols ) {
+            this.content.append(message.substring(0, this.maxSymbols-this.content.length()));
+            return;
+        }
+        this.content.append(message);
+    }
+
+    public void show() {
+        System.out.println(content);
+    }
+}
---
+++ java/Pen.java
@@ -0,0 +1,40 @@
+public class Pen {
+    private int inkAmount;
+    private int inkCapacity;
+
+    public Pen(int inkCapacity) {
+        this.inkAmount = inkCapacity;
+        this.inkCapacity = inkCapacity;
+    }
+
+    public Pen() {
+        this(1024);
+    }
+
+    public int getInkAmount() {
+        return this.inkAmount;
+    }
+
+    public int getInkCapacity() {
+        return this.inkCapacity;
+    }
+
+    public void write(Paper paper, String message)
+        throws OutOfInkException, OutOfSpaceException {
+
+        if ( this.inkAmount == 0 ) {
+            throw new OutOfInkException();
+        }
+        if ( message.length() > this.inkAmount ) {
+            paper.addContent(message.substring(0, this.inkAmount));
+            this.inkAmount = 0;
+            return;
+        }
+        paper.addContent(message);
+        this.inkAmount -= message.length();
+    }
+
+    public void refill() {
+        inkAmount = inkCapacity;
+    }
+}
